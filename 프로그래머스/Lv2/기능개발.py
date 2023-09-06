7507c9a3d9f7c4d5c0059fd49150a0f9665d3f40
Yebin Kim <rladpqls07@gmail.com>
2023-09-06 17:17:14 +0900

    Update 기능개발.py

--- 프로그래머스/Lv2/기능개발.py
+++ 프로그래머스/Lv2/기능개발.py
@@ -1,29 +1,28 @@
 def solution(progresses, speeds):
     answer = []
-    left = len(progresses) # 앞으로 배포해야 할 기능 개수
+    left = len(progresses) # 배포해야 할 총 기능 수
+    day = 1 # 작업 일 수
+    release = 0 # 하루에 배포 가능한 기능 수
     
     while left > 0:
-        release = 0 # 당일 배포 가능한 기능 개수
-        
-        # 당일 작업량 반영
-        for i in range(left):
-            progresses[i] += speeds[i]
-        
-        # 진도율 100% 달성한 기능 배포
-        while left > 0 and progresses[0] >= 100:
+        if progresses[0] + (day * speeds[0]) >= 100:
             progresses.pop(0); speeds.pop(0)
             release += 1; left -= 1
-        
-        # 당일 배포한 기능 개수 기록
-        if release != 0:
-            answer.append(release)
-    
+        else:
+            if release > 0:
+                answer.append(release)
+            release = 0
+            day += 1
+
+    # 마지막 날 배포
+    if release > 0:
+        answer.append(release)
+            
     return answer
 
 """
 
-시간 복잡도를 고려하지 않은 너무 정직한 풀이.
-n의 개수가 크지 않아서 실행 속도에 문제는 없었지만, 문제에서 주어진 조건을 너무 그대로 구현하려고 하는 것보다는
-유연하게 효율적인 풀이를 생각하는 습관을 들여야 할 듯하다.
+이전에 작성했던 중첩 반복문 코드 대신 반복문을 한 번만 사용하는 코드로 수정한 버전
+직접 배열의 값을 업데이트하는 것이 아니라 시간을 고려한 계산을 통해 값을 시뮬레이션 해줬다.
 
 """
