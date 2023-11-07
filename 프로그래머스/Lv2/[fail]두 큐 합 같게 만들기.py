b94dbe326d298a76da2b1e30b0fd1b159c7bfa24
Yebin Kim <rladpqls07@gmail.com>
2023-11-07 15:44:39 +0900

    Update [fail]두 큐 합 같게 만들기.py

--- 프로그래머스/Lv2/[fail]두 큐 합 같게 만들기.py
+++ 프로그래머스/Lv2/[fail]두 큐 합 같게 만들기.py
@@ -58,4 +58,55 @@ def solution(queue1, queue2):
 => 추가 반례를 찾아야 함
 
 """
+
+
+# 3rd. 타임아웃, 정확성 96.7 (11)
+from collections import deque
+
+def solution(queue1, queue2):
+    turn = 0
+    # 수행시간 단축을 위해 deque 자료구조 활용
+    queue1 = deque(queue1)
+    queue2 = deque(queue2)
+    first_q1 = deque(queue1)
     
+    # 수행시간 단축을 위해 정수 연산으로 합계 관리
+    sum1 = sum(queue1); sum2 = sum(queue2)
+    
+    # 만약 두 정수 배열 원소들의 합이 홀수라면 똑같이 값을 나누는 것이 불가함
+    if (sum1 + sum2) % 2 != 0:
+        return -1
+    
+    while sum1 != sum2:
+        # 더 이상 넘겨줄 값이 없으면 불가한 것으로 간주
+        if (len(queue1) == 0 or len(queue2) == 0):
+            return -1
+        
+        # 두 큐 합이 반복되는 경우
+        if turn > 1 and (queue1 == first_q1):
+            return -1
+        
+        # 합이 큰 쪽에서 작은 쪽으로 원소를 넘겨줌
+        if sum1 > sum2:
+            num = queue1.popleft()
+            queue2.append(num)
+            sum1 -= num; sum2 += num
+        else:
+            num = queue2.popleft()
+            queue1.append(num)
+            sum1 += num; sum2 -= num
+        turn += 1
+    
+    return turn
+
+"""
+타임아웃의 원인으로 보이는 sum 연산의 반복을 없애고
+수행시간 단축을 위해 deque 자료구조 활용.
+
+대부분의 타임아웃 테스트케이스를 통과했지만, 11번만 예외로 남았다.
+
+<두 큐 합이 반복되는 경우> 처리 코드를 삭제했을 때 11번과 28번에서만 타임아웃이 발생하는 걸로 봐서
+무한루프가 발생하는 또다른 반례가 아닐까 예상..
+
+예외 처리 보안 필요.
+"""
