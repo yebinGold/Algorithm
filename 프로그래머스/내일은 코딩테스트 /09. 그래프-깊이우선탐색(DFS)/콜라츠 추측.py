5e190cad1ca79b24035dec5ae11c801bed946fe5
Yebin Kim <rladpqls07@gmail.com>
2023-11-20 16:21:06 +0900

    Update 콜라츠 추측.py

--- 프로그래머스/내일은 코딩테스트 /09. 그래프-깊이우선탐색(DFS)/콜라츠 추측.py
+++ 프로그래머스/내일은 코딩테스트 /09. 그래프-깊이우선탐색(DFS)/콜라츠 추측.py
@@ -1,10 +1,9 @@
-1) 반복문
+# 1) 반복문
 def solution(num):
     if num == 0:
         return 0
     
     cnt = 0
-    
     while num != 1:
         if num % 2 == 0:
             num //= 2
@@ -16,3 +15,23 @@ def solution(num):
             return -1
 
     return cnt
+
+
+# 2) 재귀함수
+def solution(num):
+    if num == 0:
+        return 0
+
+    return dfs(num, 0)
+
+
+def dfs(num, cnt):
+    if num == 1:
+        return cnt
+    if cnt >= 500:
+        return -1
+    
+    if num % 2 == 0:
+        return dfs(num // 2, cnt + 1)
+    else:
+        return dfs(num * 3 + 1, cnt + 1)
